e19fe63d4d072d07040e433e472593decefcd1f9
Mohit Vashist <vashistmohit20@gmail.com>
2024-07-05 03:14:16 +0530

    leetcode 287

---
+++ DAY2/finding_duplicate_array.java
@@ -0,0 +1,22 @@
+package DAY2;
+
+public class finding_duplicate_array {
+        public int findDuplicate(int[] nums) {
+            int slow=nums[0];
+            int fast=nums[0];
+            do{
+                slow=nums[slow];
+                fast=nums[nums[fast]];
+            }while(slow!=fast);
+    
+            fast=nums[0];
+            while(slow!=fast)
+            {
+            slow=nums[slow];
+            fast=nums[fast];
+            }
+    
+            return slow;
+            
+        }
+}
