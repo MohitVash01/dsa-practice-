358fbb27b1b1e9e136c99586226373c8cde67ab2
MohitVash01 <164342116+MohitVash01@users.noreply.github.com>
2024-07-09 00:06:09 +0530

    Leet Code 128

--- Day4/Longest_Consecutive_Subsequence.java
+++ Day4/Longest_Consecutive_Subsequence.java
@@ -1,31 +1,36 @@
-package Day 4;
+import java.util.HashSet;
 
-public class long {
-    class Solution {
-        public int longestConsecutive(int[] arr) {
-            int n=arr.length;
-            if(n==0) return 0;
-            int longest=1;
-            HashSet<Integer> hs=new HashSet<>();
-            for(int i=0;i<arr.length;i++){
-                hs.add(arr[i]);
-            }
-            for(Integer it: hs)
-            {
-                if(hs.contains(it-1) == hs.hasNext())
-                {
-                    int cnt=1;
-                    int x=it;
-                    while(hs.contains(x+1) != hs.hasNext())
-                    {
-                        x=x+1;
-                        cnt=cnt+1;
-                    }
-                    longest=Math.max(longest,cnt);
-                }
-            }
-            return longest;
+class Solution {
+    public int longestConsecutive(int[] arr) {
+        int n = arr.length;
+        if (n == 0) return 0;
+
+        int longest = 1;
+        HashSet<Integer> hs = new HashSet<>();
+
+        // Add all elements to the HashSet
+        for (int i = 0; i < n; i++) {
+            hs.add(arr[i]);
         }
+
+        // Iterate through the elements in the HashSet
+        for (Integer it : hs) {
+            // Check if it is the start of a sequence
+            if (!hs.contains(it - 1)) {
+                int cnt = 1;
+                int x = it;
+
+                // Count the length of the sequence
+                while (hs.contains(x + 1)) {
+                    x = x + 1;
+                    cnt = cnt + 1;
+                }
+
+                // Update the longest length found
+                longest = Math.max(longest, cnt);
+            }
+        }
+
+        return longest;
     }
-    
 }
