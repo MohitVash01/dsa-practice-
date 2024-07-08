006359923af5c208216020eee58385f54ab80f27
MohitVash01 <164342116+MohitVash01@users.noreply.github.com>
2024-07-08 23:10:12 +0530

    Leet code 18

--- Day4/Four_sum.java
+++ Day4/Four_sum.java
@@ -1,23 +1,25 @@
-class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
+import java.util.*;
+
+ public class Four_Sum {
+    public List<List<Integer>> fourSum(int[] arr, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) continue;
-            for (int j = i + 1; j < nums.length; j++) {
-                if (j != i + 1 && nums[j] == nums[j - 1]) continue;
+        if (arr == null || arr.length < 4) return ans;
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length - 3; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) continue; // Skip duplicates for i
+            for (int j = i + 1; j < arr.length - 2; j++) {
+                if (j > i + 1 && arr[j] == arr[j - 1]) continue; // Skip duplicates for j
                 int k = j + 1;
-                int l = nums.length - 1;
+                int l = arr.length - 1;
                 while (k < l) {
-                    long sum = nums[i] + nums[j];
-                    sum += nums[k] ;
-                    sum += nums[l];
+                    long sum = (long) arr[i] + arr[j] + arr[k] + arr[l];
                     if (sum == target) {
-                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        ans.add(Arrays.asList(arr[i], arr[j], arr[k], arr[l]));
                         k++;
                         l--;
-                        while (k < l && nums[k] == nums[k - 1]) k++;
-                        while (k < l && nums[l] == nums[l + 1]) l--;
+                        while (k < l && arr[k] == arr[k - 1]) k++; // Skip duplicates for k
+                        while (k < l && arr[l] == arr[l + 1]) l--; // Skip duplicates for l
                     } else if (sum < target) {
                         k++;
                     } else {
