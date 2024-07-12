ad86192d502e43fe497e4d51fdc5e79d6728e3e2
MohitVash01 <164342116+MohitVash01@users.noreply.github.com>
2024-07-13 03:47:23 +0530

    Remove_N_th_Node_From_End.java

--- Day5/Remove_N_th_Node_From_End.java
+++ Day5/Remove_N_th_Node_From_End.java
@@ -1,20 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(n==1 && head.next==null)      return null;
-        ListNode slow = head;
-        ListNode fast = head;
-        for(int i=0 ; i<n ; i++)
+        ListNode start=new ListNode();
+        start.next=head;
+        ListNode slow=start;
+        ListNode fast=start;
+
+        for(int i=0;i<n;i++)
         {
-            fast = fast.next;
+            fast=fast.next;
         }
-        if(fast==null)
-            return head.next;
-        while(fast.next != null)
+        while(fast.next!=null)
         {
-            slow = slow.next;
-            fast = fast.next;
+            fast=fast.next;
+            slow=slow.next;
         }
-        slow.next = slow.next.next;
-    return head;
+        slow.next=slow.next.next;
+
+        return start.next;
+        
+        
     }
 }
