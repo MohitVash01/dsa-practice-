401495266db79845c25baa0dd77eca85a043339b
MohitVash01 <164342116+MohitVash01@users.noreply.github.com>
2024-07-12 22:36:19 +0530

    Leet code 876-Middle_Of_LL.java

--- Day5/Middle_Of_LL.java
+++ Day5/Middle_Of_LL.java
@@ -1,13 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode slow=  head;
-        ListNode fast = head;
-        while(fast.next != null && fast.next.next != null)
+        int len=0;
+        ListNode temp =head;
+        ListNode slow =head;
+        ListNode fast =head;
+        while(temp!=null)
         {
-            slow = slow.next;
-            fast = fast.next.next;
-        } 
-        if(fast.next.next == null) return slow.next;
-    return slow;
+            len++;
+            temp=temp.next;
+        }
+        while(fast.next!=null && fast.next.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        if(len%2==0) return slow.next;
+        else return slow;
     }
 }
