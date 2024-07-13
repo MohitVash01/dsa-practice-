86bd5d9ff1de18d26a93f58d6d47b5132e856cf1
MohitVash01 <164342116+MohitVash01@users.noreply.github.com>
2024-07-13 23:31:04 +0530

    (LEETCODE2)Add_Two_LL.java

--- Day5/Add_Two_Number.java
+++ Day5/Add_Two_Number.java
@@ -1,31 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carry = 0;
-        ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
-        ListNode head1 = l1;
-        ListNode head2 = l2;
-        while(head1 !=null || head2!=null || carry ==1)
-        {
-            int sum = 0;
-            if(head1 !=null)
-            {
-                sum += head1.val;
-                head1 = head1.next;
-            }
-            if(head2 != null)
-            {
-                sum += head2.val;
-                head2 = head2.next;
-            }
-            sum += carry;
-            carry = sum/10;
-    
-            ListNode new_node = new ListNode(sum%10);
-            temp.next = new_node;
-            temp = temp.next;
+        ListNode dummy=new ListNode();
+        ListNode temp=dummy;
+        int carry=0;
 
+        while(l1!=null || l2!=null || carry==1){
+            int sum=0;
+            if(l1!=null)
+            {
+                sum += l1.val;
+                l1=l1.next;
+            }
+
+            if(l2!=null)
+            {
+                sum += l2.val;
+                l2=l2.next;
+            }
+            sum=sum+carry;
+            carry=sum/10;
+            ListNode currnode=new ListNode(sum%10);
+            temp.next=currnode;
+            temp=temp.next;
         }
-    return dummy.next;
+        return dummy.next;
     }
 }
