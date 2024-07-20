26e7a7e8bd2801bf3c39ebcffb27fb2e38d8d42e
MohitVash01 <164342116+MohitVash01@users.noreply.github.com>
2024-07-20 20:34:39 +0530

    Create Rearrange_Array_Element.java
    leetcode = 2149

---
+++ Day4/Rearrange_Array_Element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] rearrangeArray(int[] ar) {
+         int pos = 0;
+        int neg = 1;
+        int temp [] = new int[ar.length];
+        for(int i=0 ; i<ar.length ; i++)
+        {
+            if(ar[i] >0)
+            {
+                temp[pos] = ar[i];
+                pos +=2;
+            }
+            else
+            {
+                temp[neg] = ar[i];
+                neg+=2;
+            }
+        }
+        return temp;
+    }
+}
