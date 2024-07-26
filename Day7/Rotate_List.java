f0293789cc68f3690393190e2785c9b78736bc7e
MohitVash01 <164342116+MohitVash01@users.noreply.github.com>
2024-07-26 23:06:36 +0530

    Rotate_List.java

--- Day7/Rotate_List.java
+++ Day7/Rotate_List.java
@@ -1,28 +1,34 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-    if(head == null||head.next == null||k == 0) return head;
-    ListNode temp = head;
-    int length = 1;
-    while(temp.next != null) {
-        ++length;
-        temp = temp.next;
-    }
-    temp.next = head;
-    k = k%length;
-    int end = length-k; // to find how much ahead we need to shift head
-    ListNode new_head = head;
-    ListNode prev = new_head;
-    while(end != 0)
-    {
-        end--;
-        prev = new_head;
-        new_head = new_head.next;
-    }
-    head = new_head;
-    prev.next= null;
-        
-    return head;
+        if(head==null || head.next==null || k==0) return head;
 
+        ListNode cur=head;
+        int len=1;
+        while(cur.next!=null)
+        {
+            len++;
+            cur=cur.next;
+        }
+        
+        cur.next=head;
+        k=k%len;
+        k=len-k;
+
+        while(k-- > 0) cur=cur.next;
+
+        head=cur.next;
+        cur.next=null;
+
+        return head;
     }
 }
